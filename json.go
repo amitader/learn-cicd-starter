37c516a5ffa972d07345dd17fe1c259a9ee11fd2
amitader <amit.adler12@gmail.com>
2025-08-14 15:51:58 +0300

    add cd

--- json.go
+++ json.go
@@ -31,8 +31,6 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	}
 	w.WriteHeader(code)
 	if _, err := w.Write(dat); err != nil {
-		log.Printf("Error writing response: %v", err)
-		w.WriteHeader(500)
-		return
+		log.Printf("Critical error writing response: %s", err)
 	}
 }
--- main.go
+++ main.go
@@ -92,7 +92,7 @@ func main() {
 	srv := &http.Server{
 		Addr:        ":" + port,
 		Handler:     router,
-		ReadTimeout: 10 * time.Second,
+		ReadTimeout: 5 * time.Second,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
