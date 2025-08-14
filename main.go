5f30aed07f7d5b0d58bdd3ad63f261241481cab1
amitader <amit.adler12@gmail.com>
2025-08-14 13:16:51 +0300

    add linte checks

--- main.go
+++ main.go
@@ -97,9 +97,7 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
-
-
 func unused() {
-    // this function does nothing
-    // and is called nowhere
+	// this function does nothing
+	// and is called nowhere
 }
